b26e9d51055fb7d9bdcc177593cc38318659427e
wanghaotianGitHub <m19834325668@163.com>
2019-04-21 17:20:22 +0000

    aa

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    YangGe
+                    Wangtian
                 </div>
 
                 <div class="links">
